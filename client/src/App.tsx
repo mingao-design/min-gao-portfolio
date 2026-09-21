5362cc7cb084f57d9e32cea1d48ba7c516984c5e
mingao-design <2.06709502e+08+mingao-design@users.noreply.github.com>
2026-09-21 22:00:39 +0000

    Remove obsolete drawing route reference

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,6 @@ import Writing from "./pages/Writing";
 import ResearchDetail from "./pages/ResearchDetail";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Drawing from "./pages/Drawing";
 import Annex from "./pages/Annex";
 
 
@@ -23,7 +22,6 @@ function Routes() {
     <SiteShell><Switch>
       <Route path={"/"} component={Home} />
       <Route path={"/projects"} component={Projects} />
-      <Route path={"/drawing"} component={Drawing} />
       <Route path={"/annexe"} component={Annex} />
       <Route path={"/projects/:slug"} component={ProjectDetail} />
       <Route path={"/writing/:slug"} component={ResearchDetail} />
